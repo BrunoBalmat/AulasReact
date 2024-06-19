eebdb90fc509bb332165180692ab91e0b43a5252
BrunoBalmat <111462798+BrunoBalmat@users.noreply.github.com>
2024-06-19 10:26:51 -0300

    exercicio de useState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import {Louco} from './utils'; 
 import './App.css';
-import React from 'react';
-
-
+import React, { useState } from 'react';
 
 function App() {
 
+  const [teste1, setTeste1] = useState('fulano')
+  const [teste2, setTeste2] = useState('ciclano')
+  const [multiplicador, setMultiplicador] = useState(1)
+
+  const funcao = () => {
+
+    if (teste1 === 'fulano') {
+      setTeste2('beltrano')
+    }
+    if (teste2 === 'beltrano') {
+      setTeste1('Fernando')
+    }
+    if (teste1 === 'Fernando') {
+      setTeste1('fulano')
+      setTeste2('ciclano')
+    }
+    setMultiplicador(multiplicador * 4)
+  }
   return (
-    <form>
-      <input onChange={(e) => {Louco(e.target.value)}}/>
-      <button onClick={(e) =>{}}>Enviar</button>
-    </form>
+    <>
+      <button onClick={() => {funcao()}}>Botão</button>
+       O {teste1} com {teste2} e mais {multiplicador}
+    </>
   );
 }
-
 export default App;
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,28 +1,4 @@
-// export const Louco = (nome) =>{
-//     if(nome === 'Guitarra'){
-//         console.log('Tem cordas')        
-//         }
-//         else if(nome === 'Bateria'){
-//             console.log('Tem pedal')
-//         }
-//         else if(nome === "Microfone"){
-//             console.log('Tem fios')
-//         }
-//         else{
-//             console.log('Não conheço esse instrumento')
-//         }
-// return
-// }
 
-export const Louco =(nome)=> {
-    switch(nome){
-     case "Bruno": console.log('É muito chato'); break;
-     case "Fulano": console.log('É feio'); break;
-     case "Beltrano": console.log('É gordo'); break;
-     default : console.log('Não conheço'); break;
-    }
- }
- 
  
 
      
