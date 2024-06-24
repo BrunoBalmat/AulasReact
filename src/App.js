ede8bbee9f0d51939dda9267a460c805f58be513
BrunoBalmat <111462798+BrunoBalmat@users.noreply.github.com>
2024-06-24 10:05:49 -0300

    api de post

--- src/App.js
+++ src/App.js
@@ -4,33 +4,65 @@ import axios from 'axios'
 
 function App() {
 
-const [resposta, setResposta] =useState()
+  const [resposta, setResposta] = useState()
+  const [tipo, setTipo] = useState()
+  const [cor, setCor] = useState()
+  const [valor, setValor] = useState()
 
   // fetch('https://concessionaria-1111f-default-rtdb.firebaseio.com/roupas/.json')
   // .then(response => response.json())
   // .then(data => setResposta(data)); 
 
-    useEffect(()=>{
+  useEffect(() => {
     axios.get('https://concessionaria-1111f-default-rtdb.firebaseio.com/roupas/.json')
-    .then(function (response) {
-      setResposta(response.data);
-    })})
-    
-  return (
-      <div>
+      .then(function (response) {
+        setResposta(response.data);
+      })
+  })
 
-        {resposta?.masculinas.tipo}       
-        {
-          Object.values(resposta.femininas).map(roupa =>{
-            return(
+  const cadastro = () => {
+    axios.post('https://concessionaria-1111f-default-rtdb.firebaseio.com/roupas/femininas/.json', {
+      cor: cor,
+      tipo: tipo,
+      valor: valor
+    })
+      .then(function (response) {
+        alert('Roupa cadastrada')
+      }).catch(function () {
+        alert('roupa não cadastrada')
+      })
+
+  }
+
+
+  return (
+    <div>
+
+      {resposta?.masculinas.tipo}
+      {
+        resposta && Object.values(resposta?.femininas).map(roupa => {
+          return (
+            <>
               <p>{roupa?.tipo}</p>
-            )         
-          })
-        }
-      </div>
+              <p>{roupa?.cor}</p>
+              <p>{roupa?.valor}</p>
+            </>
+          )
+        })
+      }
+      <h2>cadastro de roupas</h2>
+      <fomr>
+        <label> Tipo: </label>
+        <input onChange={(e) => setTipo(e.target.value)} />
+        <label> Cor: </label>
+        <input onChange={(e) => setCor(e.target.value)} />
+        <label> Valor: </label>
+        <input onChange={(e) => setValor(e.target.value)} />
+        <button onClick={() => cadastro()}>Cadastrar</button>
+      </fomr>
+    </div>
   )
 }
 export default App;
 
- 
-         
+
