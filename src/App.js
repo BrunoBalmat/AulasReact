6c198016cdc56babb0135e64397ec2451fcd3c73
BrunoBalmat <111462798+BrunoBalmat@users.noreply.github.com>
2024-06-24 09:11:02 -0300

    exercio de get

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ const [resposta, setResposta] =useState()
 
   // fetch('https://concessionaria-1111f-default-rtdb.firebaseio.com/roupas/.json')
   // .then(response => response.json())
-  // .then(data => setResposta(data));
+  // .then(data => setResposta(data)); 
+
     useEffect(()=>{
     axios.get('https://concessionaria-1111f-default-rtdb.firebaseio.com/roupas/.json')
     .then(function (response) {
       setResposta(response.data);
-    })
-}, [])
+    })})
+    
   return (
       <div>
+
         {resposta?.masculinas.tipo}       
         {
           Object.values(resposta.femininas).map(roupa =>{
